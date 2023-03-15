9ecf63d26a7f8b765b54623fac48a282f6344ff1
Vitaliy <sprinter-100m@ukr.net>
2023-03-15 22:01:41 +0200

    first commit

--- food/models.py
+++ food/models.py
@@ -1,3 +1,94 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    # Категории
+    name = models.CharField('Название', max_length=250)
+    description = models.TextField('Описание')
+    url = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Manufacturer(models.Model):
+    # Производитель
+    name = models.CharField('Имя', max_length=100)
+    description = models.TextField('Описание')
+    image = models.ImageField('Изображение', upload_to='manufacturer/')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Производитель'
+        verbose_name_plural = 'Производители'
+
+
+class Product(models.Model):
+    # Продукты
+    title = models.CharField('Название', max_length=150)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    weight = models.FloatField('Вес', blank=True, default=0)
+    photo = models.ImageField('Изображение', upload_to='Products/')
+    draft = models.BooleanField('В наличии', default=False)
+    url = models.SlugField(max_length=150, unique=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, verbose_name='Категории')
+    manufacturer = models.ManyToManyField(Manufacturer, verbose_name='Производитель')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class RetingsStar(models.Model):
+    # Звезда рейтинга
+    value = models.PositiveSmallIntegerField('Значение', default=0)
+
+    def __str__(self):
+        return self.value
+    
+    class Meta:
+        verbose_name = 'Звезда рейтинга'
+        verbose_name_plural = 'Звезды рейтинга'
+
+
+class Reting(models.Model):
+    # Рейтинг
+    ip = models.CharField('IP адресс', max_length=50)
+    star = models.ForeignKey(RetingsStar, on_delete=models.CASCADE, verbose_name='Звезда')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукт')
+
+    def __str__(self):
+        return self.star
+    
+    class Meta:
+        verbose_name = 'Рейтинг'
+        verbose_name_plural = 'Рейтинги'
+
+
+class Comments(models.Model):
+    # Коментарий
+    name = models.CharField('Имя', max_length=150)
+    text = models.TextField('Коментфрий', max_length=5000)
+    email = models.EmailField()
+    parent = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, blank=True, null=True, verbose_name='Родитель')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукты')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
--- food/views.py
+++ food/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import *
 
-# Create your views here.
+
+
+class ProductsView(ListView):
+    model = Product
+    queryset = Product.objects.filter(draft=False)
+    template_name = 'food/product_list.html'
