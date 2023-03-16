795ee4dd72f1edd87dce58e9b23f152b8ac85872
Vitaliy <sprinter-100m@ukr.net>
2023-03-16 19:00:25 +0200

    adminka

--- food/admin.py
+++ food/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import *
+from django.utils.safestring import mark_safe
 
 
 @admin.register(Category)
 # Категории
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'url')
+    prepopulated_fields = {'url' : ('name',)}
 
 
 class CommentsInline(admin.StackedInline):
@@ -17,20 +19,34 @@ class CommentsInline(admin.StackedInline):
 @admin.register(Manufacturer)
 # Производитель
 class ManufacturerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'image')
+    list_display = ('name', 'description', 'get_image')
     list_display_links = ('name',)
+    readonly_fields = ('image',)
+
+    def get_image(self, objects):
+        return mark_safe(f"<img src={objects.image.url} width='50' ")
+    
+    get_image.short_description = 'Изображение'
 
 
 @admin.register(Product)
 # Продукты
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
-        'title', 'description', 'price', 'weight', 'photo', 'draft', 'url')
+        'title', 'description', 'price', 'weight', 'get_photo', 'draft', 'url')
     list_display_links = ('title',)
     list_filter = ('title', 'price')
     search_fields = ('title', 'category__name')
     list_editable = ('draft',)
     inlines = [CommentsInline]
+    readonly_fields = ('photo',)
+
+    def get_photo(self, objects):
+        return mark_safe(f"<img src={objects.photo.url} width='50' ")
+    
+    get_photo.short_description = 'Изображение'
+
+    prepopulated_fields = {'url' : ('title',)}
 
 
 @admin.register(RetingsStar)
