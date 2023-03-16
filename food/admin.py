1e852afa1208b29addf1bb9fdbbace3c1c64dcf3
Vitaliy <sprinter-100m@ukr.net>
2023-03-16 18:43:26 +0200

    adminka

--- food/admin.py
+++ food/admin.py
@@ -6,4 +6,43 @@ from .models import *
 # Категории
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'url')
+
+
+class CommentsInline(admin.StackedInline):
+    model = Comments
+    extra = 1
+    readonly_fields = ('name', 'email')
+
+
+@admin.register(Manufacturer)
+# Производитель
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'image')
     list_display_links = ('name',)
+
+
+@admin.register(Product)
+# Продукты
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'description', 'price', 'weight', 'photo', 'draft', 'url')
+    list_display_links = ('title',)
+    list_filter = ('title', 'price')
+    search_fields = ('title', 'category__name')
+    list_editable = ('draft',)
+    inlines = [CommentsInline]
+
+
+@admin.register(RetingsStar)
+# Звезда рейтинга
+class RetingsStarAdmin(admin.ModelAdmin):
+    list_display = ('value',)
+    list_display_links = ('value',)
+
+
+@admin.register(Comments)
+# Коментарий
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'text', 'email')
+    list_display_links = ('name',)
+    readonly_fields = ('name', 'email')
