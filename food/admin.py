c64f590f5b1fde447e11c16831af187543c46d62
Vitaliy <sprinter-100m@ukr.net>
2023-03-16 18:38:17 +0200

    adminka

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Category)
+# Категории
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'url')
+    list_display_links = ('name',)
