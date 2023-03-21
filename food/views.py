ccca506fa68bf5afb3cf75b4c4cc402b07d27570
Vitaliy <sprinter-100m@ukr.net>
2023-03-21 13:24:33 +0200

    datail products

--- food/views.py
+++ food/views.py
@@ -7,7 +7,6 @@ from .models import *
 class ProductsView(ListView):
     model = Product
     queryset = Product.objects.filter(draft=False)
-    template_name = 'food/product_list.html'
 
 
 class Search(ListView):
