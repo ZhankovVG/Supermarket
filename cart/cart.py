41bdf00a8044193a5dc920535715cf018e2a807a
Vitaliy <sprinter-100m@ukr.net>
2023-04-20 16:00:34 +0300

    AddCart

--- cart/cart.py
+++ cart/cart.py
@@ -6,20 +6,16 @@ from food.models import Product
 class Cart(object):
 
     def __init__(self, request):
-        """
-        Инициализация корзины
-        """
+        # Инициализация корзины
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            # сохраняем ПУСТУЮ корзину в сессии
+        # сохраняем ПУСТУЮ корзину в сессии
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
     def __iter__(self):
-        """
-        Перебираем товары в корзине и получаем товары из базы данных.
-        """
+        # Перебираем товары в корзине и получаем товары из базы данных
         product_ids = self.cart.keys()
         # получаем товары и добавляем их в корзину
         products = Product.objects.filter(id__in=product_ids)
@@ -34,15 +30,11 @@ class Cart(object):
             yield item
 
     def __len__(self):
-        """
-        Считаем сколько товаров в корзине
-        """
+        # Считаем сколько товаров в корзине
         return sum(item['quantity'] for item in self.cart.values())
 
     def add(self, product, quantity=1, update_quantity=False):
-        """
-        Добавляем товар в корзину или обновляем его количество.
-        """
+        # Добавляем товар в корзину или обновляем его количество
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0,
@@ -58,9 +50,7 @@ class Cart(object):
         self.session.modified = True
 
     def remove(self, product):
-        """
-        Удаляем товар
-        """
+        # Удаляем товар
         product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
--- cart/views.py
+++ cart/views.py
@@ -3,10 +3,13 @@ from django.views.decorators.http import require_POST
 from food.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+from food.models import Category
+
 
 
 @require_POST
 def cart_add(request, product_id):
+    # добавление продуктов в корзину
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     form = CartAddProductForm(request.POST)
@@ -19,6 +22,7 @@ def cart_add(request, product_id):
 
 
 def cart_remove(request, product_id):
+    # удаление продуктов из корзины
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
@@ -26,8 +30,10 @@ def cart_remove(request, product_id):
 
 
 def cart_detail(request):
+    # то что находится в корзине
     cart = Cart(request)
+    categories = Category.objects.all()
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
                                                                    'update': True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+    return render(request, 'cart/detail.html', {'cart': cart, 'categories': categories})
